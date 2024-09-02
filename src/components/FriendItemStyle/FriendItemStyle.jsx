096baf242be1e35431958c5d064a5c44e2f58799
NataliiaPleskanka <154823178+NataliiaPleskanka@users.noreply.github.com>
2024-09-02 15:52:16 +0200

    changes

--- src/components/FriendItemStyle/FriendItemStyle.jsx
+++ src/components/FriendItemStyle/FriendItemStyle.jsx
@@ -1,6 +1,11 @@
+import clsx from "clsx";
 import css from "./FriendItemStyle.module.css";
 
 export default function FriendItemStyle({ variant, children }) {
-  const className = `${css.status} ${css[variant]}`;
+  const className = clsx(
+    css.status,
+    variant === "online" ? css.online : css.offline
+  );
+
   return <p className={className}>{children}</p>;
 }
